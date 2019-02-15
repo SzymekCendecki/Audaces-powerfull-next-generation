d2931a97a9946150fb504ee05a8ce1e879fdbdbd
SzymonCendecki <31997216+SzymekCendecki@users.noreply.github.com>
2019-02-15 11:05:52 +0100

    Delete workingFile.js

--- js/js/workingFile.js
+++
@@ -1,9 +0,0 @@
-let intro = require("./intro.js");
-let heroCreator = require("./heroCreator.js");
-
-document.addEventListener("DOMContentLoaded", () => {
-  $("#info, #licence, #tutorial, #game, #mainDescription").hide();
-  $("#randomHero, #manualHero").hide();
-  intro.intro();
-  heroCreator.heroCreator();
-});
